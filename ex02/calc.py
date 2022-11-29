d63c75540377c6b055ee2662590dbfa1eada821e
nozomi <c0a21065a6@edu.teu.ac.jp>
2022-11-29 15:25:53 +0900

    四則演算と小数点の追加

--- ex02/calc.py
+++ ex02/calc.py
@@ -18,33 +18,35 @@ def button_click(event):
 
 # 練習１
 root = tk.Tk()
-root.geometry("300x500")
+root.geometry("400x600")
 
 #練習4
 entry = tk.Entry(root, justify = "right", width = 10, font = ("", 40))
 entry.grid(row = 0, column = 0, columnspan = 3)
 
 # 練習２
-r, c = 1, 0
+r, c = 1, 2
 for num in range(9, -1, -1):
     button = tk.Button(root, text=f"{num}", width=4, height=2, font=("", 30))
     button.grid(row=r, column=c)
     button.bind("<1>", button_click)
-    c += 1
-    if c%3 == 0:
+    c -= 1
+    if c < 0:
         r += 1
-        c = 0
+        c = 2
 
 #練習5
-operators = ["+", "="]
+r = 1
+c = 3
+operators = ["+", "-", "/", "*", "=", "."]
 for ope in operators:
     button = tk.Button(root, text=f"{ope}", width=4, height=2, font=("", 30))
     button.grid(row=r, column=c)
     button.bind("<1>", button_click)
-    c += 1
-    if c%3 == 0:
-        r += 1
-        c = 0
+    r += 1
+    if r == 5:
+        r -= 1
+        c -= 1
 
 
 root.mainloop()

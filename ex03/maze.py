a1ebf45cb94507d3d63f18ba8a5643309180e965
nozomi <c0a21065a6@edu.teu.ac.jp>
2022-12-06 14:42:46 +0900

    練習3-7

--- ex03/maze.py
+++ ex03/maze.py
@@ -9,6 +9,22 @@ def key_up(event):
     global key
     key = ""
 
+def main_proc():
+    global cx, cy
+    if key == "Up":
+        cy -= 20
+
+    elif key == "Down":
+        cy += 20
+    
+    elif key == "Left":
+        cx -= 20
+
+    elif key == "Right":
+        cx += 20
+    
+    canvas.coords("kokaton", cx, cy)
+    root.after(100 , main_proc)
 
 if __name__ == "__main__":
     root = tk.Tk()
@@ -23,4 +39,6 @@ if __name__ == "__main__":
     key = ""
     root.bind("<KeyPress>", key_down)
     root.bind("<KeyRelease>", key_up)
+
+    main_proc()
     root.mainloop()

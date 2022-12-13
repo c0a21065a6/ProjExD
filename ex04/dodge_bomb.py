fc49f9c6c1b98181214e589884ce9b8326489057
nozomi <c0a21065a6@edu.teu.ac.jp>
2022-12-13 14:12:42 +0900

    練習4-2

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -2,12 +2,16 @@ import pygame as pg
 import sys
 
 def main():
-
+    #練習１
     pg.display.set_caption("逃げろ！こうかとん")
     scrn_sfc = pg.display.set_mode((1600, 900))
-
     pgbg_sfc = pg.image.load("fig/pg_bg.jpg")
-    
+    #練習2
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                return
+
 
 if __name__ == "__main__":
     pg.init()

d5b312fe36a434a2a0e175449503dce2e6c9ddf1
nozomi <c0a21065a6@edu.teu.ac.jp>
2022-12-13 14:20:14 +0900

    練習4-3

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -7,12 +7,21 @@ def main():
     #練習１
     pg.display.set_caption("逃げろ！こうかとん")
     scrn_sfc = pg.display.set_mode((1600, 900))
+    
     pgbg_sfc = pg.image.load("fig/pg_bg.jpg")
     pgbg_rct = pgbg_sfc.get_rect()
     
+    #練習３
+    tori_sfc = pg.image.load("fig/6.png")
+    tori_sfc = pg.transform.rotozoom(tori_sfc, 0, 2.0)
+    tori_rct = tori_sfc.get_rect()
+    tori_rct.center = 900, 400
+    scrn_sfc.blit(tori_sfc, tori_rct)
+
     #練習2
     while True:
         scrn_sfc.blit(pgbg_sfc, pgbg_rct)
+        
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
